cf6b7ddbeb79f4f13abb9afb9eeee43e20020a0c
HasanKhurram150 <hasankhurram150@gmail.com>
2025-07-24 18:44:47 +0500

    feat: unneccessary files removal

--- src/components/placement-list/columns.ts
+++
@@ -1,8 +0,0 @@
-export const placementColumns = [
-  { id: "1", header: "Placement Code", className: "min-w-[15rem]" },
-  { id: "2", header: "Placement Name", className: "min-w-[12rem]" },
-  { id: "3", header: "Form", className: "min-w-[8.125rem]" },
-  { id: "4", header: "Size", className: "min-w-[15rem]" },
-  { id: "5", header: "Material", className: "min-w-[12rem]" },
-  { id: "6", header: "Inventory", className: "min-w-[10rem]" },
-];
--- src/components/settings/columns.ts
+++
@@ -1,8 +0,0 @@
-export const proPackageColumns = [
-  { id: "1", header: "Interval", className: "min-w-[15rem]" },
-  { id: "2", header: "Amount", className: "min-w-[15rem]" },
-  // { id: "3", header: "Currency", className: "min-w-[10rem]" },
-  // { id: "4", header: "Stripe Price ID", className: "min-w-[15rem]" },
-  { id: "3", header: "Action", className: "min-w-[10rem]" },
-];
-
